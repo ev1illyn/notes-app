bac9e076c5f9cc1ceba8c74bf0e5b45cc9248a3a
ev1illyn <evillyndsoliveiras@gmail.com>
2021-04-19 18:13:10 -0300

    feat: dragging and dropping tasks between columns

--- src/initial-data.js
+++ src/initial-data.js
@@ -2,17 +2,28 @@ const initialData = {
     tasks: {
         'task-1': { id: 'task-1', content: 'play volleyball' },
         'task-2': { id: 'task-2', content: 'learn react' },
-        'task-3': { id: 'task-3', content: 'work on GEDE' },
-        'task-4': { id: 'task-4', content: 'drink coke' }
+        'task-3': { id: 'task-3', content: 'work' },
+        'task-4': { id: 'task-4', content: 'watch the walking dead show' },
+        'task-5': { id: 'task-5', content: 'drink coke' }
     },
     columns: {
         'column-1': {
             id: 'column-1',
             title: 'To do',
-            taskIds: ['task-1', 'task-2', 'task-3', 'task-4'],
+            taskIds: ['task-3'],
+        },
+        'column-2': {
+            id: 'column-2',
+            title: 'Doing',
+            taskIds: ['task-2', 'task-4'],
+        },
+        'column-3': {
+            id: 'column-3',
+            title: 'Done',
+            taskIds: ['task-1', 'task-5'],
         },
     },
-    columnOrder: ['column-1'],
+    columnOrder: ['column-1', 'column-2', 'column-3'],
 };
 
 export default initialData;
