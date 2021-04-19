441731f86b15514134d67356f4262f3a9447ede3
ev1illyn <evillyndsoliveiras@gmail.com>
2021-04-18 21:56:23 -0300

    feat: draggable/reording list

---
+++ src/index.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import '@atlaskit/css-reset';
+import initialData from './initial-data';
+import Column from "./column";
+import { DragDropContext } from "react-beautiful-dnd";
+
+class App extends React.Component {
+
+  state = initialData;
+
+  onDragEnd = result => {
+
+  }
+
+  render() {
+    return (
+
+      <DragDropContext onDragEnd={this.onDragEnd}>
+
+        {
+          this.state.columnOrder.map(columnId => {
+
+            const column = this.state.columns[columnId];
+            const tasks = column.taskIds.map(taskId => this.state.tasks[taskId]);
+
+            return <Column key={column.id} column={column} tasks={tasks} />;
+
+          })
+        }
+      </DragDropContext>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
