134da93922ef802c6b7886d791f25e8d85473c26
ev1illyn <evillyndsoliveiras@gmail.com>
2021-05-11 18:50:42 -0300

    feat: add tasks in initial column

---
+++ src/add-task.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+
+export default function AddTask({ addTask }) {
+
+    const [task, setTask] = useState({ data: "" });
+
+
+    // funcao executada a cada tecla digitada
+    const handleInputChange = e => {
+        setTask({ data: e.target.value });
+    };
+
+    // funcao executada ao teclar enter
+    const submitTask = e => {
+
+        e.preventDefault();
+        addTask(task.data);
+        setTask({ data: "" });
+        //console.log(task);
+        //console.log('---------');
+
+    };
+
+    return (
+        <div className="form-group">
+            <form onSubmit={submitTask}>
+                <input
+                    className="form-control"
+                    type="text"
+                    value={task.data}
+                    placeholder="Type new task..."
+                    onSubmit={submitTask}
+                    onChange={handleInputChange}
+                ></input>
+            </form>
+        </div>
+    );
+}
